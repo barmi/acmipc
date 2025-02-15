997566fa482bb00da44f75f9b865f2d052d442af
skshin <skshin@gmail.com>
2025-02-15 11:38:53 +0900

    1557: 속도개선

--- diamond/5/1557/main.cpp
+++ diamond/5/1557/main.cpp
@@ -5,45 +5,65 @@
 // 1557:       https://www.acmicpc.net/problem/1557
 
 #include <iostream>
-#include <set>
-#include <cmath>
+#include <deque>
+#include <vector>
 
 using namespace std;
 
+using ll = long long;
+
+deque<bool> sieve()
+{
+    deque<bool> p(40558, true);
+    p[0] = p[1] = false;
+    for (int i = 2; i < p.size(); ++i) {
+        if (p[i]) {
+            for (int j = 2 * i; j < p.size(); j += i) {
+                p[j] = false;
+            }
+        }
+    }
+    return p;
+}
+
+vector<int> P;
+
+ll solve(ll X, int idx, ll val)
+{
+    ll ret = X / (val * val);
+    for (int i = idx; i < P.size(); ++i) {
+        ll y = val * P[i];
+        if (X < y * y) {
+            break;
+        }
+        ret -= solve(X, i + 1, y);
+    }
+    return ret;
+}
 
 int main()
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    int K;
+    deque<bool> p = sieve();
+    for (int i = 2; i < p.size(); ++i) {
+        if (p[i]) {
+            P.push_back(i);
+        }
+    }
+    int K{};
     cin >> K;
-
-    set<uint64_t> squares;
-    int K2 = K * 2;
-    int Kh = K / 2 + 1;
-    for (uint64_t i = 2; i <= sqrt(K2)*2; ++i) {
-        squares.insert(i * i);
-
-        if (squares.size() > K) {
-            break;
-        }
-        for (int j = 2; j <= sqrt(K2)*2; ++j) {
-            if (i * i * j > Kh) {
-                break;
-            }
-            squares.insert(i * i * j);
+    ll lo = 0, hi = 2e9;
+    while (lo + 1 < hi) {
+        ll mid = (lo + hi) / 2;
+        ll s = solve(mid, 0, 1);
+        if (s < K) {
+            lo = mid;
+        } else {
+            hi = mid;
         }
     }
-
-    int count = 0;
-    for (auto s: squares) {
-        if (s > K + count) {
-            break;
-        }
-        count++;
-    }
-    cout << K + count << "\n";
-
+    cout << hi << "\n";
     return 0;
 }
