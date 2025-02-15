e376d40f140cb294dc38e83bfe523757abf8a776
skshin <skshin@gmail.com>
2025-02-15 16:00:03 +0900

    1008: 추가

---
+++ bronze/5/1008/1008.cpp
@@ -0,0 +1,24 @@
+//
+// Created by skshin on 2025. 2. 15..
+//
+
+// https://solved.ac/class?class=1
+// https://www.acmicpc.net/problem/1008
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int A, B;
+    cin >> A >> B;
+
+    cout.precision(10);
+    cout << (double)A / B << "\n";
+
+    return 0;
+}
