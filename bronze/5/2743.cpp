5523190623909d2a7679fdfc9af5bd780f16e010
skshin <skshin@gmail.com>
2025-11-18 16:00:34 +0900

    add 2743 (브론즈5)

---
+++ bronze/5/2743.cpp
@@ -0,0 +1,17 @@
+//
+// Created by skshin on 2025. 9. 13..
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string s;
+	cin >> s;
+	cout << s.length() << "\n";
+
+	return 0;
+}
