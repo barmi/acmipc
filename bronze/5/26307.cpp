7a96c5746c4672f64911eed8c77d98be213ab281
skshin <skshin@gmail.com>
2025-11-23 12:01:03 +0900

    add 26307 (브론즈5)

---
+++ bronze/5/26307.cpp
@@ -0,0 +1,14 @@
+//
+// Created by skshin on 2025. 11. 21..
+//
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int HH, MM;
+    cin >> HH >> MM;
+    cout << (HH-9) * 60 + MM << "\n";
+
+    return 0;
+}
